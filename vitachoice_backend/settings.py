14874cf62e6ef823021d6f1de8a3cb055168e434
carnage999-max <jamesezekiel039@gmail.com>
2025-10-09 21:16:33 +0100

    updated settings.py: changed DEBUG to False

--- vitachoice_backend/settings.py
+++ vitachoice_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["vita-choice-backend.onrender.com", "localhost"]
 CORS_ALLOWED_ORIGINS = [
